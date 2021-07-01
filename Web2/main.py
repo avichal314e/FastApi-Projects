3b40d4e521bef62909289f556266f4a9f9ca616c
Avichal Srivastava <avichal.srivastava@314ecorp.com>
2021-07-01 17:04:57 +0530

    Added part 2 of Web Application Project

--- Web2/main.py
+++ Web2/main.py
@@ -1,22 +1,32 @@
+# Part 1 - Create a Python Rest API endpoint
 # Create a CRUD Rest API endpoint for Healthcare Provider directory. Use Python - FastAPI for building the Rest APIs.
 # Store the data as an in-memory Dictionary.
+
+# Part 2 - Persist the data in a file
+# For the above use case, now serialise the objects and persist them in a file.
 
 # Invokation: hypercorn main:app --reload
 
 
+from enum import Enum
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uuid
+import pickle
 
 
 app = FastAPI()
-
 db = {}
 
 
+class status(int, Enum):
+    active = 1
+    inactive = 0
+
+
 class Provider(BaseModel):
-    active: Optional[bool] = True
+    active: Optional[status] = 1
     name: str
     qualification: str
     speciality: str
@@ -27,6 +37,23 @@ class Provider(BaseModel):
     address: str
 
 
+@app.on_event("startup")
+def on_startup():
+    global db
+    try:
+        with open('database', 'rb') as database:
+            db = pickle.load(database)
+    except:
+        pass
+
+
+@app.on_event("shutdown")
+def on_shutdown():
+    global db
+    with open('database', 'wb') as database:
+        pickle.dump(db, database)
+
+
 @app.get('/')
 def index():
     return 'HEALTH PROVIDER API ACTIVE!!'
@@ -34,13 +61,10 @@ def index():
 
 def get_uuid(param: str, value: str):
     if(param == 'active'):
-        if(value == "false"):
-            value = False
-        elif(value == 'true'):
-            value = True
-        else:
+        try:
+            value = int(value)
+        except:
             return []
-
     results = []
     for uuid in db:
         try:
@@ -58,20 +82,14 @@ def get_providers():
 
 @app.get('/get_provider/{field}={value}')
 def get_provider(field: str, value: str):
-    if(field == "providerID"):
-        try:
-            return db[uuid.UUID(value)]
-        except:
-            return "No Data Found"
-    else:
-        entries = get_uuid(field, value)
-        results = [db[entry] for entry in entries]
-        return (results if results else "No Data Found")
+    entries = get_uuid(field, value)
+    results = [db[entry] for entry in entries]
+    return (results if results else "No Data Found")
 
 
-@app.put('/provider')
+@app.put('/create_provider')
 def create_provider(provider: Provider):
-    pro_uuid = uuid.uuid4()
+    pro_uuid = str(uuid.uuid4())
     db[pro_uuid] = provider.dict()
     db[pro_uuid]['providerID'] = pro_uuid
     return db[pro_uuid]
@@ -79,58 +97,48 @@ def create_provider(provider: Provider):
 
 @app.delete('/delete_provider/{field}={value}')
 def delete_provider(field: str, value: str):
-    if(field == "providerID"):
-        try:
-            val = db[uuid.UUID(value)]
-            del db[uuid.UUID(value)]
-            return [val]
-        except:
-            return "No Data Found"
-    else:
-        entries = get_uuid(field, value)
-        results = []
-        for entry in entries:
-            results.append(db[entry])
-            del db[entry]
-        return (results if results else "No Data Found")
+    entries = get_uuid(field, value)
+    results = []
+    for entry in entries:
+        results.append(db[entry])
+        del db[entry]
+    return (results if results else "No Data Found")
 
 
 @app.post('/update_provider/{id}')
 def update_provider(id: str, active=None, name=None, qualification=None, speciality=None, phone=None, department=None, organization=None, location=None, address=None):
-    try:
-        pro_uuid = uuid.UUID(id)
-    except:
-        return "Invalid UUID!"
-    if(pro_uuid in db.keys()):
-        if(active):
-            if(active == 'false'):
-                db[pro_uuid]['active'] = False
-            else:
-                db[pro_uuid]['active'] = True
+    if(id in db.keys()):
+        try:
+            if(active and int(active) not in [0, 1]):
+                return "Invalid Active Status!"
+            db[id]['active'] = (
+                int(active) if active is not None else db[id]['active'])
+        except:
+            return "Invalid Active Status!"
 
-        db[pro_uuid]['name'] = (
-            name if name is not None else db[pro_uuid]['name'])
+        db[id]['name'] = (
+            name if name is not None else db[id]['name'])
 
-        db[pro_uuid]['qualification'] = (
-            qualification if qualification is not None else db[pro_uuid]['qualification'])
+        db[id]['qualification'] = (
+            qualification if qualification is not None else db[id]['qualification'])
 
-        db[pro_uuid]['speciality'] = (
-            speciality if speciality is not None else db[pro_uuid]['speciality'])
+        db[id]['speciality'] = (
+            speciality if speciality is not None else db[id]['speciality'])
 
-        db[pro_uuid]['phone'] = (
-            phone if phone is not None else db[pro_uuid]['phone'])
+        db[id]['phone'] = (
+            phone if phone is not None else db[id]['phone'])
 
-        db[pro_uuid]['department'] = (
-            department if department is not None else db[pro_uuid]['department'])
+        db[id]['department'] = (
+            department if department is not None else db[id]['department'])
 
-        db[pro_uuid]['organization'] = (
-            organization if organization is not None else db[pro_uuid]['organization'])
+        db[id]['organization'] = (
+            organization if organization is not None else db[id]['organization'])
 
-        db[pro_uuid]['location'] = (
-            location if location is not None else db[pro_uuid]['location'])
+        db[id]['location'] = (
+            location if location is not None else db[id]['location'])
 
-        db[pro_uuid]['address'] = (
-            address if address is not None else db[pro_uuid]['address'])
+        db[id]['address'] = (
+            address if address is not None else db[id]['address'])
         return "SUCCESS!! Provider Details Updated."
     else:
         return "Provider not found!!"
